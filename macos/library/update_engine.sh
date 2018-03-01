c7131d1a1736018e40730731c9b007322f116acf
Stuart Morgan <stuartmorgan@google.com>
2018-03-01 15:27:32 -0800

    Style fixes for update_engine.sh
    Cleanup to better follow Google style guide for bash scripts.

--- macos/library/update_engine.sh
+++ macos/library/update_engine.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 #
 # Copyright 2018 Google LLC
 #
@@ -24,53 +24,60 @@
 # "flutter_engine_framework" directory next to both of them.
 # TODO: Make the script more flexible about paths.
 
-if [ "$SRCROOT" != "" ]; then
-  PROJECT_DIR="$SRCROOT"
+set -e
+
+if [[ -n "${SRCROOT}" ]]; then
+  PROJECT_DIR="${SRCROOT}"
 else
   PROJECT_DIR="$(dirname "$0")"
 fi
+readonly PROJECT_DIR
 # Path to the folder containing this repository and the flutter tree (see
 # note above).
-REPO_PARENT="${PROJECT_DIR}/../../.."
-FLUTTER_ENGINE_VERSION_FILE=flutter/bin/internal/engine.version
+readonly REPO_PARENT="${PROJECT_DIR}/../../.."
+readonly FLUTTER_ENGINE_VERSION_FILE=flutter/bin/internal/engine.version
 # By default, match the embedder engine version to the current flutter tree.
 # This can be manually overridden with FLUTTER_ENGINE_VERSION_OVERRIDE (e.g.,
 # to roll the embedder to a newer-but-still-compatible version).
-if [ "$FLUTTER_ENGINE_VERSION_OVERRIDE" == "" ]; then
-  FLUTTER_EMBEDDER_SHA=`cat $REPO_PARENT/$FLUTTER_ENGINE_VERSION_FILE`
-else
+if [[ -n "${FLUTTER_ENGINE_VERSION_OVERRIDE}" ]]; then
   FLUTTER_EMBEDDER_SHA="${FLUTTER_ENGINE_VERSION_OVERRIDE}"
+else
+  FLUTTER_EMBEDDER_SHA=$(cat ${REPO_PARENT}/${FLUTTER_ENGINE_VERSION_FILE})
 fi
+readonly FLUTTER_EMBEDDER_SHA
 # The name of the Flutter engine framework.
-FRAMEWORK_FILENAME=FlutterEmbedder.framework
+readonly FRAMEWORK_FILENAME=FlutterEmbedder.framework
 
 # Everything from here on assumes that the current path is $ENGINE_DIRECTORY.
 # This avoids having to worry about paths being relative vs. absolute.
-ENGINE_DIRECTORY="${REPO_PARENT}/flutter_engine_framework"
-mkdir -p "$ENGINE_DIRECTORY"
-cd "$ENGINE_DIRECTORY"
+readonly ENGINE_DIRECTORY="${REPO_PARENT}/flutter_engine_framework"
+mkdir -p "${ENGINE_DIRECTORY}"
+cd "${ENGINE_DIRECTORY}"
 
 # TODO: Eliminate this file once https://github.com/flutter/flutter/issues/13879
 # is fixed.
-VERSION_STAMP_FILENAME=.last_engine_version
+readonly VERSION_STAMP_FILENAME=.last_engine_version
 
 # Only download the framework if the version has changed.
-if [ -e "${FRAMEWORK_FILENAME}" ] && [ -e "${VERSION_STAMP_FILENAME}" ]; then
-  LAST_DOWNLOAD_VERSION=`cat ${VERSION_STAMP_FILENAME}`
+if [[ -e "${FRAMEWORK_FILENAME}" && -e "${VERSION_STAMP_FILENAME}" ]]; then
+  LAST_DOWNLOAD_VERSION=$(cat "${VERSION_STAMP_FILENAME}")
+  readonly LAST_DOWNLOAD_VERSION
 fi
 
-if [ "$LAST_DOWNLOAD_VERSION" != "$FLUTTER_EMBEDDER_SHA" ]; then
-  FRAMEWORK_ARCHIVE_FILENAME="${FRAMEWORK_FILENAME}.zip"
+if [[ "${LAST_DOWNLOAD_VERSION}" != "${FLUTTER_EMBEDDER_SHA}" ]]; then
+  readonly FRAMEWORK_ARCHIVE_FILENAME="${FRAMEWORK_FILENAME}.zip"
 
-  curl -O https://storage.googleapis.com/flutter_infra/flutter/${FLUTTER_EMBEDDER_SHA}/darwin-x64/$FRAMEWORK_ARCHIVE_FILENAME
-  rm -rf $FRAMEWORK_FILENAME
-  # Unzip twice, as the downloaded zip contains a zip file, rather than the framework.
-  unzip -o $FRAMEWORK_ARCHIVE_FILENAME && unzip -o $FRAMEWORK_ARCHIVE_FILENAME -d $FRAMEWORK_FILENAME
-  rm $FRAMEWORK_ARCHIVE_FILENAME
+  curl -O "https://storage.googleapis.com/flutter_infra/flutter/${FLUTTER_EMBEDDER_SHA}/darwin-x64/${FRAMEWORK_ARCHIVE_FILENAME}"
+  rm -rf "${FRAMEWORK_FILENAME}"
+  # Unzip twice, as the downloaded zip contains a zip file, rather than the
+  # framework.
+  unzip -o "${FRAMEWORK_ARCHIVE_FILENAME}" && \
+    unzip -o "${FRAMEWORK_ARCHIVE_FILENAME}" -d "${FRAMEWORK_FILENAME}"
+  rm "${FRAMEWORK_ARCHIVE_FILENAME}"
 
   # Record the new download version.
-  echo $FLUTTER_EMBEDDER_SHA > $VERSION_STAMP_FILENAME
-  echo "Downloaded $FRAMEWORK_FILENAME version '$FLUTTER_EMBEDDER_SHA'."
+  echo "${FLUTTER_EMBEDDER_SHA}" > "${VERSION_STAMP_FILENAME}"
+  echo "Downloaded ${FRAMEWORK_FILENAME} version '${FLUTTER_EMBEDDER_SHA}'."
 else
-  echo "$FRAMEWORK_FILENAME version '$FLUTTER_EMBEDDER_SHA' already present."
+  echo "${FRAMEWORK_FILENAME} version '${FLUTTER_EMBEDDER_SHA}' already present."
 fi
