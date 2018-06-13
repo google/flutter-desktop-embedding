6684ea437905dfb8e1b582cacf4f3d175e62fea3
Stuart Morgan <stuartmorgan@google.com>
2018-06-13 11:54:50 -0700

    Clean up example flutter app
    Adds an analysis file, fixes issues the linter settings flagged, and does some
    Dart 2 and boilerplate cleanup.
    
    Preparation for changes to the example app to include plugins, so that plugin
    changes can be easily tested.

--- example_flutter/test/widget_test.dart
+++ example_flutter/test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:example_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
 
