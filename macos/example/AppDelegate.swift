f4c1b472062d578d21ea036bb0b64dc2f05da334
Stuart Morgan <stuartmorgan@google.com>
2018-06-21 12:09:17 -0700

    [macOS] Quit the example app when closing the window
    The application is not multi-window, so it should quit when the window
    is closed.

--- macos/example/AppDelegate.swift
+++ macos/example/AppDelegate.swift
@@ -17,5 +17,9 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   @IBOutlet weak var window: NSWindow!
+
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
 }
 
