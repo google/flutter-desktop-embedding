cb9fe6d72fd9e5b017930759ca1297471a68dbc9
Stuart Morgan <stuartmorgan@google.com>
2018-08-20 20:22:52 -0700

    Minor cleanup
    - Add missing license header to new Dart file.
    - Resolve a Dart analyzer warning.
    - Revert changes to the now-removed pubspec.lock to avoid conflicts.

--- example_flutter/lib/keyboard_test_page.dart
+++ example_flutter/lib/keyboard_test_page.dart
@@ -1,3 +1,16 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
@@ -26,7 +39,7 @@ class _KeyboardTestPageState extends State<KeyboardTestPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-          title: new Text("Keyboard events test"),
+          title: new Text('Keyboard events test'),
           leading: new IconButton(
               icon: new Icon(Icons.arrow_back),
               onPressed: () {
