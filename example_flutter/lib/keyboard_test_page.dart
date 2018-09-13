e1d264b698604910380e86a237cd4b1e95093b08
廖宇浩 <taiyangniaoming@foxmail.com>
2018-09-13 18:03:33 +0800

    Remove redundant dependencies

--- example_flutter/lib/keyboard_test_page.dart
+++ example_flutter/lib/keyboard_test_page.dart
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
