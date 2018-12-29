d5583a75eb5c48075104d256da211f6e246586e9
James Clarke <james@clarkezone.net>
2018-12-29 08:36:08 -0800

    Restore echo off to build_jsonlib.bat

--- library/windows/scripts/build_jsonlib.bat
+++ library/windows/scripts/build_jsonlib.bat
@@ -11,7 +11,7 @@
 :: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 :: See the License for the specific language governing permissions and
 :: limitations under the License.
-::@echo off
+@echo off
 
 :: Find where VS lives and start a VC command prompt
 set pre=Microsoft.VisualStudio.Product.
