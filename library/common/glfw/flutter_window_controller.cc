129614e66992d6b068211c04ac58fa8a83aae86d
Stuart Morgan <stuartmorgan@google.com>
2019-01-29 17:38:28 -0500

    [linux/windows] Separate out a C channel API
    Eliminates PluginRegistrar and BinaryMessenger from the embedder.h API
    surface, moving them up to the FlutterWindowController level. In their
    place, adds new low-level messaging APIs, which are based closely on
    the Flutter engine embedder APIs (including an equivalent message
    struct), in some cases wrapping them directly. Unlike the engine APIs,
    they allow per-channel callbacks, so individual plugins can use the API.
    
    Since PluginHandler is no longer the source of truth for callback
    registration, only an adaptor, it's now fine to have multiple instances
    of them, all passing through to the underlying C API.
    
    Part of #230

--- library/common/glfw/flutter_window_controller.cc
+++ library/common/glfw/flutter_window_controller.cc
@@ -16,6 +16,8 @@
 
 #include <iostream>
 
+#include "library/common/internal/plugin_handler.h"
+
 namespace flutter_desktop_embedding {
 
 FlutterWindowController::FlutterWindowController(std::string &icu_data_path)
@@ -56,8 +58,14 @@ PluginRegistrar *FlutterWindowController::GetRegistrarForPlugin(
   if (!window_) {
     return nullptr;
   }
-  return flutter_desktop_embedding::GetRegistrarForPlugin(window_, plugin_name);
-}
+  if (!plugin_handler_) {
+    plugin_handler_ = std::make_unique<PluginHandler>(window_);
+  }
+  // Currently, PluginHandler acts as the registrar for all plugins, so the
+  // name is ignored. It is part of the API to reduce churn in the future when
+  // aligning more closely with the Flutter registrar system.
+  return plugin_handler_.get();
+}  // namespace flutter_desktop_embedding
 
 void FlutterWindowController::RunEventLoop() {
   if (window_) {
