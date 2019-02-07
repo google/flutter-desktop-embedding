2a3942a9bb514a60a5688925826bb941f6118175
stuartmorgan <stuart.morgan@gmail.com>
2019-02-07 14:33:20 -0800

    Make the Windows example build self-contained (#275)
    Copies the necessary Flutter data to a known location relative to the
    executable, and uses executable-relative paths rather than
    working-directory-relative paths.
    
    This allows the executable to be run from any location, and makes it
    clear what must be bundled as part of a working example. Follows the
    same basic structure as the Linux example's bundling.

--- example/windows_fde/flutter_embedder_example.cpp
+++ example/windows_fde/flutter_embedder_example.cpp
@@ -18,10 +18,43 @@
 
 #include "flutter_desktop_embedding/flutter_window_controller.h"
 
+// Include windows.h last, to minimize potential conflicts. The CreateWindow
+// macro needs to be undefined because it prevents calling
+// FlutterWindowController's method.
+#include <windows.h>
+#undef CreateWindow
+
+namespace {
+
+// Returns the path of the directory containing this executable, or an empty
+// string if the directory cannot be found.
+std::string GetExecutableDirectory() {
+  char buffer[MAX_PATH];
+  if (GetModuleFileName(nullptr, buffer, MAX_PATH) == 0) {
+    std::cerr << "Couldn't locate executable" << std::endl;
+    return "";
+  }
+  std::string executable_path(buffer);
+  size_t last_separator_position = executable_path.find_last_of('\\');
+  if (last_separator_position == std::string::npos) {
+    std::cerr << "Unabled to find parent directory of " << executable_path
+              << std::endl;
+    return "";
+  }
+  return executable_path.substr(0, last_separator_position);
+}
+
+}  // namespace
+
 int main(int argc, char **argv) {
-  // TODO: Make paths relative to the executable so it can be run from anywhere.
-  std::string assets_path = "..\\build\\flutter_assets";
-  std::string icu_data_path = "..\\build\\windows_fde\\icudtl.dat";
+  // Resources are located relative to the executable.
+  std::string base_directory = GetExecutableDirectory();
+  if (base_directory.empty()) {
+    base_directory = ".";
+  }
+  std::string data_directory = base_directory + "\\data";
+  std::string assets_path = data_directory + "\\flutter_assets";
+  std::string icu_data_path = data_directory + "\\icudtl.dat";
 
   // Arguments for the Flutter Engine.
   std::vector<std::string> arguments;
