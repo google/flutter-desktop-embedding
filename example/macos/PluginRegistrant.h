4e92fe42ed6ea23633f293521ceded4cd0289791
Stuart Morgan <stuartmorgan@google.com>
2019-04-17 10:32:54 -0700

    Move macOS plugin registration to a separate file
    Matches the structure of the iOS GeneratedPluginRegistrant. Uses ObjC,
    rather than Swift, so that changes to add a new plugin are localized to
    one file, instead of also affecting the bridging header.
    
    This will make it easier to eventually transition to generated plugin
    registration, and in the meantime makes it slightly easier to add
    plugins manually.

--- example/macos/PluginRegistrant.h
+++ example/macos/PluginRegistrant.h
@@ -1,4 +1,4 @@
-// Copyright 2018 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,4 +13,7 @@
 // limitations under the License.
 
 #import <FlutterMacOS/FlutterMacOS.h>
-#import "PluginRegistrant.h"
+
+@interface PluginRegistrant : NSObject
++ (void)registerWithRegistry:(NSObject<FLEPluginRegistry>*)registry;
+@end
