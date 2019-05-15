ff2f59fe736ef7da55ff46ee21286fb9f90c95e3
stuartmorgan <stuartmorgan@google.com>
2019-05-15 19:40:37 -0400

    Update Windows build for Flutter changes (#384)
    Updates for compatibility with flutter/flutter#32783
    - Simplifies the build scripts, and adds --local-engine support.
    - Replaces name_output.bat with a line in the existing scripts to write
    flutter\exe_filename.
    
    Removes the instructions page about how to use a local engine with FDE
    projects, as all platforms now use the normal Flutter process.

--- example/windows/scripts/bundle_assets_and_deps.bat
+++ example/windows/scripts/bundle_assets_and_deps.bat
@@ -13,32 +13,31 @@
 :: limitations under the License.
 @echo off
 
-set FLUTTER_APP_DIR=%~dp0..\..
-set ICU_DATA_SOURCE=%FLUTTER_ROOT%\bin\cache\artifacts\engine\windows-x64\icudtl.dat
-set ASSET_DIR_NAME=flutter_assets
-
-set FLUTTER_LIBRARY_DIR=%~1
+set FLUTTER_CACHE_DIR=%~1
 set BUNDLE_DIR=%~2
+set EXE_NAME=%~3
+
 set DATA_DIR=%BUNDLE_DIR%data
-set TARGET_ASSET_DIR=%DATA_DIR%\%ASSET_DIR_NAME%
 
 if not exist "%DATA_DIR%" call mkdir "%DATA_DIR%"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-:: Build the Flutter assets.
-cd "%FLUTTER_APP_DIR%"
-call %FLUTTER_ROOT%\bin\flutter build bundle %EXTRA_BUNDLE_FLAGS%
-if %errorlevel% neq 0 exit /b %errorlevel%
-:: Copy them to the data directory.
+:: Write the executable name to the location expected by the Flutter tool.
+echo %EXE_NAME%>"%FLUTTER_CACHE_DIR%exe_filename"
+
+:: Copy the Flutter assets to the data directory.
+set FLUTTER_APP_DIR=%~dp0..\..
+set ASSET_DIR_NAME=flutter_assets
+set TARGET_ASSET_DIR=%DATA_DIR%\%ASSET_DIR_NAME%
 if exist "%TARGET_ASSET_DIR%" call rmdir /s /q "%TARGET_ASSET_DIR%"
 if %errorlevel% neq 0 exit /b %errorlevel%
 call xcopy /s /e /i /q "%FLUTTER_APP_DIR%\build\%ASSET_DIR_NAME%" "%TARGET_ASSET_DIR%"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Copy the icudtl.dat file from the Flutter tree to the data directory.
-call xcopy /y /d /q "%ICU_DATA_SOURCE%" "%DATA_DIR%"
+call xcopy /y /d /q "%FLUTTER_CACHE_DIR%icudtl.dat" "%DATA_DIR%"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Copy the Flutter DLL to the target location.
-call xcopy /y /d /q "%FLUTTER_LIBRARY_DIR%flutter_windows.dll" "%BUNDLE_DIR%"
+call xcopy /y /d /q "%FLUTTER_CACHE_DIR%flutter_windows.dll" "%BUNDLE_DIR%"
 if %errorlevel% neq 0 exit /b %errorlevel%
