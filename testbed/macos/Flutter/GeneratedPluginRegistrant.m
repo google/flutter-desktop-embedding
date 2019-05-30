676515c3dea0bdcc0e9bb33f3ca1b4acd240b359
Stuart Morgan <stuartmorgan@google.com>
2019-05-30 09:59:05 -0700

    More WIP

---
+++ testbed/macos/Flutter/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <example_plugin/ExamplePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ExamplePlugin registerWithRegistrar:[registry registrarForPlugin:@"ExamplePlugin"]];
+}
+
+@end
