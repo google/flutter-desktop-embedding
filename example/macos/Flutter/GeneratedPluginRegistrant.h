a11d442802bdece7b5c56621fef1703cf51f6f8b
Stuart Morgan <stuartmorgan@google.com>
2019-05-30 16:21:10 -0700

    Switch example to generated plugin registrant

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.h
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#ifndef GeneratedPluginRegistrant_h
+#define GeneratedPluginRegistrant_h
+
+#import <FlutterMacOS/FlutterMacOS.h>
+
+@interface GeneratedPluginRegistrant : NSObject
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry;
+@end
+
+#endif /* GeneratedPluginRegistrant_h */
