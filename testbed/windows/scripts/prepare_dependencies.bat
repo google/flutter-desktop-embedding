6b105be508bf8fef239fb1b6bdb91d2b9279bb0d
stuartmorgan <stuartmorgan@google.com>
2019-06-02 12:53:03 -0700

    Quote call to tool_backend in Windows build (#416)
    The call was missing quoting, which will be an issue if FLUTTER_ROOT
    contains spaces.

--- testbed/windows/scripts/prepare_dependencies.bat
+++ testbed/windows/scripts/prepare_dependencies.bat
@@ -15,4 +15,4 @@
 
 :: Run flutter tool backend.
 set BUILD_MODE=%~1
-%FLUTTER_ROOT%\packages\flutter_tools\bin\tool_backend windows-x64 %BUILD_MODE%
+"%FLUTTER_ROOT%\packages\flutter_tools\bin\tool_backend" windows-x64 %BUILD_MODE%
