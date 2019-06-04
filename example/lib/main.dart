2f892318a6b1ec8b6405928f091a9f33fb1519bc
stuartmorgan <stuart.morgan@gmail.com>
2019-06-04 15:55:19 -0700

    Fix some minor analyzer issues (#401)
    Fixes a couple of issues that have slipped in, and removes a deprecated
    setting.
    
    Also removes the folder-local analysis_options.yaml files from the two
    example directories, since they are intended at this point to be copied
    and re-used elsewhere, so referring out to the root yaml doesn't make
    sense.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:io' show Platform;
-
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
 import 'package:flutter/material.dart';
