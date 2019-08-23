2932e25986430a773125369d5d7ebf63140bd9c7
stuartmorgan <stuartmorgan@google.com>
2019-08-23 07:03:06 -0700

    Update Linux apps for incremental GLFW runloop (#496)
    Switches to the new API from flutter/engine#11368 and moves the GTK event handling into the application level of 'testbed'. 'example' doesn't do that handling since it doesn't use plugins. (Longer term a specific toolkit like GTK will be a core requirement of the library, so this divergence won't exist; for now this makes the simple example easier to build and run.)
    
    Note: Since the run-forever version in the C++ wrapper is still present (to avoid this change being a hard compilation break), and Windows didn't need special handling beyond what that does, Windows is not updated; it will be changing to a new API soon anyway, and leaving it avoids collisions with that work.

--- testbed/linux/testbed.cc
+++ testbed/linux/testbed.cc
@@ -13,15 +13,19 @@
 // limitations under the License.
 #include <linux/limits.h>
 #include <unistd.h>
+
 #include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <vector>
 
+// For plugin-compatible event handling (e.g., modal windows).
+#include <X11/Xlib.h>
 #include <color_panel_plugin.h>
 #include <example_plugin.h>
 #include <file_chooser_plugin.h>
 #include <flutter/flutter_window_controller.h>
+#include <gtk/gtk.h>
 #include <menubar_plugin.h>
 #include <url_launcher_fde_plugin.h>
 #include <window_size_plugin.h>
@@ -91,7 +95,17 @@ int main(int argc, char **argv) {
   WindowSizeRegisterWithRegistrar(
       flutter_controller.GetRegistrarForPlugin("WindowSize"));
 
-  // Run until the window is closed.
-  flutter_controller.RunEventLoop();
+  // Set up for GTK event handling, needed by the GTK-based plugins.
+  gtk_init(0, nullptr);
+  XInitThreads();
+
+  // Run until the window is closed, processing GTK events in parallel for
+  // plugin handling.
+  while (flutter_controller.RunEventLoopWithTimeout(
+      std::chrono::milliseconds(10))) {
+    if (gtk_events_pending()) {
+      gtk_main_iteration();
+    }
+  }
   return EXIT_SUCCESS;
 }
