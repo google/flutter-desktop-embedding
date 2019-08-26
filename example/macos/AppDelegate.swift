0aa196857e6028b4ad42bd80b7094ccafaec240d
stuartmorgan <stuartmorgan@google.com>
2019-08-26 15:33:32 -0700

    macOS template improvements (#509)
    - Make changing the application name simpler. Updates the window name and
      menu items from the plist (which is in turn populate from PRODUCT_NAME).
      Improves https://github.com/flutter/flutter/issues/39250
    - Rename ExampleWindow to be less FDE-example-specific
    
    This is likely not the full solution to application name handling, but it's a step
    in a likely direction that people can try out and evaluate.

--- example/macos/AppDelegate.swift
+++ example/macos/AppDelegate.swift
@@ -17,9 +17,32 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
   @IBOutlet weak var window: NSWindow!
+  @IBOutlet weak var applicationMenu: NSMenu!
+
+  func applicationWillFinishLaunching(_ notification: Notification) {
+    // Update UI elements to match the application name.
+    // TODO: Move this logic to a Flutter framework application delegate.
+    // See https://github.com/flutter/flutter/issues/32419.
+    let appName = applicationName()
+    window.title = appName
+    for menuItem in applicationMenu.items {
+      menuItem.title = menuItem.title.replacingOccurrences(of: "APP_NAME", with: appName)
+    }
+  }
 
   func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
+  }
+
+  /**
+   * Returns the name of the application as set in the Info.plist
+   */
+  private func applicationName() -> String {
+    var applicationName : String? = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
+    if applicationName == nil {
+      applicationName = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String
+    }
+    return applicationName!
   }
 }
 
