0ed0596bb2fc59110f33b7070d53a0e85e7c8e78
shoryukenn <naifu.guan@gmail.com>
2019-08-28 08:23:18 +0800

    modified the logic, the code is from Chromium, the project property is changed to /subsystem:windows, and the console--force-console is used to control whether to create the console. At the same time, if the parent process has a console, it will be attached directly.

--- example/windows/flutter_embedder_example.cpp
+++ example/windows/flutter_embedder_example.cpp
@@ -24,6 +24,17 @@
 #include <windows.h>
 #undef CreateWindow
 
+#include <fcntl.h>
+#include <io.h>
+#include <shellapi.h>
+#include <userenv.h>
+#include <psapi.h>
+
+#include <ios>
+#include <limits>
+#include <iostream>
+#pragma warning(disable: 4996)
+
 namespace {
 
 // Returns the path of the directory containing this executable, or an empty
@@ -44,13 +55,88 @@ std::string GetExecutableDirectory() {
   return executable_path.substr(0, last_separator_position);
 }
 
+void RouteStdioToConsole(bool create_console_if_not_found) {
+  // Don't change anything if stdout or stderr already point to a
+  // valid stream.
+  //
+  // If we are running under Buildbot or under Cygwin's default
+  // terminal (mintty), stderr and stderr will be pipe handles.  In
+  // that case, we don't want to open CONOUT$, because its output
+  // likely does not go anywhere.
+  //
+  // We don't use GetStdHandle() to check stdout/stderr here because
+  // it can return dangling IDs of handles that were never inherited
+  // by this process.  These IDs could have been reused by the time
+  // this function is called.  The CRT checks the validity of
+  // stdout/stderr on startup (before the handle IDs can be reused).
+  // _fileno(stdout) will return -2 (_NO_CONSOLE_FILENO) if stdout was
+  // invalid.
+  if (_fileno(stdout) >= 0 || _fileno(stderr) >= 0) {
+    // _fileno was broken for SUBSYSTEM:WINDOWS from VS2010 to VS2012/2013.
+    // http://crbug.com/358267. Confirm that the underlying HANDLE is valid
+    // before aborting.
+
+    intptr_t stdout_handle = _get_osfhandle(_fileno(stdout));
+    intptr_t stderr_handle = _get_osfhandle(_fileno(stderr));
+    if (stdout_handle >= 0 || stderr_handle >= 0)
+      return;
+  }
+
+  if (!AttachConsole(ATTACH_PARENT_PROCESS)) {
+    unsigned int result = GetLastError();
+    // Was probably already attached.
+    if (result == ERROR_ACCESS_DENIED)
+      return;
+    // Don't bother creating a new console for each child process if the
+    // parent process is invalid (eg: crashed).
+    if (result == ERROR_GEN_FAILURE)
+      return;
+    if (create_console_if_not_found) {
+      // Make a new console if attaching to parent fails with any other error.
+      // It should be ERROR_INVALID_HANDLE at this point, which means the
+      // browser was likely not started from a console.
+      AllocConsole();
+    }
+    else {
+      return;
+    }
+  }
+
+  // Arbitrary byte count to use when buffering output lines.  More
+  // means potential waste, less means more risk of interleaved
+  // log-lines in output.
+  enum { kOutputBufferSize = 64 * 1024 };
+
+  if (freopen("CONOUT$", "w", stdout)) {
+    setvbuf(stdout, nullptr, _IOLBF, kOutputBufferSize);
+    // Overwrite FD 1 for the benefit of any code that uses this FD
+    // directly.  This is safe because the CRT allocates FDs 0, 1 and
+    // 2 at startup even if they don't have valid underlying Windows
+    // handles.  This means we won't be overwriting an FD created by
+    // _open() after startup.
+    _dup2(_fileno(stdout), 1);
+  }
+  if (freopen("CONOUT$", "w", stderr)) {
+    setvbuf(stderr, nullptr, _IOLBF, kOutputBufferSize);
+    _dup2(_fileno(stderr), 2);
+  }
+
+  // Fix all cout, wcout, cin, wcin, cerr, wcerr, clog and wclog.
+  std::ios::sync_with_stdio();
+}
+
 }  // namespace
 
-#if !defined(WIN_CONSOLE_APP)
+
 int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE prev, wchar_t *, int) {
-#else
-int main(int argc, char **argv) {
-#endif  // WIN_CONSOLE_APP
+
+  // 1. if parent or current process has console, attach it.
+  // 2. if --force-console exist && parent or current process have not conole, 
+  // create and attach it.
+  RouteStdioToConsole(
+    lpCmdLine
+    && wcsstr(lpCmdLine, L"--force-console") != nullptr ? true : false );
+
   // Resources are located relative to the executable.
   std::string base_directory = GetExecutableDirectory();
   if (base_directory.empty()) {
