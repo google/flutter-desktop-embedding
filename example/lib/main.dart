daefecdb55d496b925f2bca83e13db25bfec78c0
jonahwilliams <jonahwilliams@google.com>
2019-09-04 19:53:02 -0700

    add inputs, outputs, and tripwire

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -68,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many t222imes:',
             ),
             Text(
               '$_counter',
---
+++ example/macos/Flutter/flutter_config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+
+touch Flutter/ephemeral/tripwire
