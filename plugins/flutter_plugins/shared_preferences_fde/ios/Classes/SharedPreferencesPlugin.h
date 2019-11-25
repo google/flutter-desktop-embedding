8c4fbd30ac357d32ce537a27d9c3261f2760937d
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2019-11-25 15:06:49 +0300

    drop dummy classes

--- plugins/flutter_plugins/shared_preferences_fde/ios/Classes/SharedPreferencesPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface SharedPreferencesPlugin : NSObject<FlutterPlugin>
-@end
--- plugins/flutter_plugins/shared_preferences_fde/ios/Classes/SharedPreferencesPlugin.m
+++
@@ -1,6 +0,0 @@
-#import "SharedPreferencesPlugin.h"
-
-@implementation SharedPreferencesPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {}
-
-@end
