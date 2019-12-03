da7256441e441937f44fe37507c0abc6ce0c6302
Brett Morgan <brett.morgan@gmail.com>
2019-12-03 15:20:01 +1030

    Reverting changes

---
+++ testbed/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import color_panel
+import example_plugin
+import file_chooser
+import menubar
+import path_provider_fde
+import shared_preferences_fde
+import url_launcher_fde
+import window_size
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ColorPanelPlugin.register(with: registry.registrar(forPlugin: "ColorPanelPlugin"))
+  ExamplePlugin.register(with: registry.registrar(forPlugin: "ExamplePlugin"))
+  FileChooserPlugin.register(with: registry.registrar(forPlugin: "FileChooserPlugin"))
+  MenubarPlugin.register(with: registry.registrar(forPlugin: "MenubarPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
+}
