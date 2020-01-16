a3892757a1dafb2219513fe60a972be28b647423
Stuart Morgan <stuartmorgan@google.com>
2020-01-16 12:56:55 -0800

    Implement remaining core methods

--- plugins/flutter_plugins/path_provider_fde/windows/path_provider_plugin.cpp
+++ plugins/flutter_plugins/path_provider_fde/windows/path_provider_plugin.cpp
@@ -13,18 +13,62 @@
 // limitations under the License.
 #include "path_provider_plugin.h"
 
-#include <windows.h>
-
+#include <ShlObj.h>
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar.h>
 #include <flutter/standard_method_codec.h>
+#include <windows.h>
+
 #include <codecvt>
 #include <memory>
+#include <sstream>
 #include <string>
 
 namespace {
 
 using flutter::EncodableValue;
+
+// Converts an null-terminated array of wchar_t's to a std::string.
+std::string StdStringFromWideChars(wchar_t *wide_chars) {
+  std::wstring_convert<std::codecvt_utf8<wchar_t>> wide_to_utf8;
+  return wide_to_utf8.to_bytes(wide_chars);
+}
+
+// Gets the path to the given folder ID, without verifying that it exists,
+// or an empty string on failure.
+std::string GetFolderPath(REFKNOWNFOLDERID folder_id) {
+  PWSTR wide_path = nullptr;
+  if (!SUCCEEDED(SHGetKnownFolderPath(folder_id, KF_FLAG_DONT_VERIFY, nullptr,
+                                      &wide_path))) {
+    return "";
+  }
+  std::string path = StdStringFromWideChars(wide_path);
+  CoTaskMemFree(wide_path);
+  return path;
+}
+
+// Returns the name of the executable, without the .exe extension, or an empty
+// string on failure.
+std::string GetExecutableName() {
+  wchar_t buffer[MAX_PATH];
+  if (GetModuleFileName(nullptr, buffer, MAX_PATH) == 0) {
+    return "";
+  }
+  std::string executable_path = StdStringFromWideChars(buffer);
+  size_t last_separator_position = executable_path.find_last_of('\\');
+  std::string executable_name;
+  if (last_separator_position == std::string::npos) {
+    executable_name = executable_path;
+  } else {
+    executable_name = executable_path.substr(last_separator_position + 1);
+  }
+  // Strip the .exe extension, if present.
+  std::string extension = ".exe";
+  if (executable_name.compare(executable_name.size() - extension.size(), extension.size(), extension) == 0) {
+    executable_name = executable_name.substr(0, executable_name.size() - extension.size());
+  }
+  return executable_name;
+}
 
 class PathProviderPlugin : public flutter::Plugin {
  public:
@@ -69,18 +113,40 @@ void PathProviderPlugin::HandleMethodCall(
   if (method_call.method_name().compare("getTemporaryDirectory") == 0) {
     TCHAR path_buffer[MAX_PATH];
     UINT length = GetTempPath(MAX_PATH, path_buffer);
-    if (length > 0 && length <= MAX_PATH) {
-      std::wstring_convert<std::codecvt_utf8<wchar_t>> wide_to_utf8;
-      std::string temp_path = wide_to_utf8.to_bytes(path_buffer);
-      flutter::EncodableValue response(temp_path.c_str());
-      result->Success(&response);
-    } else {
-      result->Error("Unable to get path");
+    if (length == 0 || length > MAX_PATH) {
+      result->Error("Unable to get temporary path");
+      return;
     }
-  } else if (method_call.method_name().compare("getApplicationSupportDirectory") == 0) {
-    result->NotImplemented();
-  } else if (method_call.method_name().compare("getApplicationDocumentsDirectory") == 0) {
-    result->NotImplemented();
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> wide_to_utf8;
+    std::string result_path = wide_to_utf8.to_bytes(path_buffer);
+    flutter::EncodableValue response(result_path);
+    result->Success(&response);
+  } else if (method_call.method_name().compare(
+                 "getApplicationSupportDirectory") == 0) {
+    std::string path = GetFolderPath(FOLDERID_RoamingAppData);
+    if (path.empty()) {
+      result->Error("Unable to get application data path");
+      return;
+    }
+    // Use the executable name as the subdirectory for now.
+    std::string exe_name = GetExecutableName();
+    if (exe_name.empty()) {
+      result->Error("Unable to get exe name");
+      return;
+    }
+    std::ostringstream response_stream;
+    response_stream << path << "\\" << exe_name;
+    flutter::EncodableValue response(response_stream.str());
+    result->Success(&response);
+  } else if (method_call.method_name().compare(
+                 "getApplicationDocumentsDirectory") == 0) {
+    std::string path = GetFolderPath(FOLDERID_Documents);
+    if (path.empty()) {
+      result->Error("Unable to get documents path");
+      return;
+    }
+    flutter::EncodableValue response(path);
+    result->Success(&response);
   } else {
     result->NotImplemented();
   }
