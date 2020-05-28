e4eb8403d1c72a70350933cc1f60aaa4d5fe3eb4
Robert Ancell <robert.ancell@canonical.com>
2020-05-28 14:07:05 +1200

    Add GTK color_panel plugin

--- testbed/gtk/flutter/gtk_plugin_registrant.cc
+++ testbed/gtk/flutter/gtk_plugin_registrant.cc
@@ -3,9 +3,10 @@
 
 #include "gtk_plugin_registrant.h"
 
+#include <color_panel/color_panel_plugin.h>
+
 void fl_register_plugins(FlPluginRegistry* registry) {
-  // Example plugin code:
-  // g_autoptr(FlPluginRegistrar) {name}_registrar =
-  // fl_plugin_registry_get_registrar_for_plugin(registry, "{class}");
-  //{name}_panel_plugin_register_with_registrar({name}_registrar);
+  g_autoptr(FlPluginRegistrar) color_panel_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "ColorPanelPlugin");
+  color_panel_plugin_register_with_registrar(color_panel_registrar);
 }
