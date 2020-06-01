507ebd5cacd5d90ec8fb4627cd8a0aac42efe9f9
stuartmorgan <stuartmorgan@google.com>
2020-06-01 11:24:33 -0700

    Update Windows testbed from template (#743)
    Picks up release mode support, and other minor template changes.

--- testbed/windows/runner/main.cpp
+++ testbed/windows/runner/main.cpp
@@ -2,32 +2,26 @@
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
 
-#include <vector>
-
 #include "flutter_window.h"
 #include "run_loop.h"
+#include "utils.h"
 #include "window_configuration.h"
 
-int APIENTRY wWinMain(_In_ HINSTANCE instance,
-                      _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t* command_line,
-                      _In_ int show_command) {
+int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
+                      _In_ wchar_t *command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
-    ::AllocConsole();
+    CreateAndAttachConsole();
   }
 
-  // Initialize COM, so that it is available for use in the library and/or plugins.
+  // Initialize COM, so that it is available for use in the library and/or
+  // plugins.
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   RunLoop run_loop;
 
   flutter::DartProject project(L"data");
-#ifndef _DEBUG
-  project.SetEngineSwitches({"--disable-dart-asserts"});
-#endif
-
   FlutterWindow window(&run_loop, project);
   Win32Window::Point origin(kFlutterWindowOriginX, kFlutterWindowOriginY);
   Win32Window::Size size(kFlutterWindowWidth, kFlutterWindowHeight);
