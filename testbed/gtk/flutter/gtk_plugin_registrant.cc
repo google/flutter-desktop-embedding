79d73d347c284a2cdae9b24ae0d875512e9d208e
Robert Ancell <robert.ancell@canonical.com>
2020-06-03 03:55:18 +1200

    Add GTK plugin loading functions (#740)
    Manually maintained versions of what will later be tool-generated files.

---
+++ testbed/gtk/flutter/gtk_plugin_registrant.cc
@@ -0,0 +1,11 @@
+// Once the Flutter tooling switches to use GTK, this will be generated
+// automatically. For now, plugins must be added here manually.
+
+#include "gtk_plugin_registrant.h"
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  // Example plugin code:
+  // g_autoptr(FlPluginRegistrar) {name}_registrar =
+  // fl_plugin_registry_get_registrar_for_plugin(registry, "{class}");
+  //{name}_panel_plugin_register_with_registrar({name}_registrar);
+}
