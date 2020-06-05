bae67ae551328695f76cd9923a7c9ea27075eba1
Robert Ancell <robert.ancell@canonical.com>
2020-06-05 17:30:54 +1200

    Use GtkApplication (#748)

--- testbed/gtk/main.cc
+++ testbed/gtk/main.cc
@@ -4,10 +4,19 @@
 #include "flutter/gtk_plugin_registrant.h"
 #include "window_configuration.h"
 
-int main(int argc, char** argv) {
-  gtk_init(&argc, &argv);
+G_DECLARE_FINAL_TYPE(FlApplication, fl_application, FL, APPLICATION,
+                     GtkApplication)
 
-  GtkWindow* window = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
+struct _FlApplication {
+  GtkApplication parent_instance;
+};
+
+G_DEFINE_TYPE(FlApplication, fl_application, GTK_TYPE_APPLICATION)
+
+// Implements GApplication::activate.
+static void fl_application_activate(GApplication* application) {
+  GtkWindow* window =
+      GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
   gtk_widget_show(GTK_WIDGET(window));
   gtk_widget_set_size_request(GTK_WIDGET(window), kFlutterWindowWidth,
                               kFlutterWindowHeight);
@@ -22,8 +31,19 @@ int main(int argc, char** argv) {
   fl_register_plugins(FL_PLUGIN_REGISTRY(view));
 
   gtk_widget_grab_focus(GTK_WIDGET(view));
+}
 
-  gtk_main();
+static void fl_application_class_init(FlApplicationClass* klass) {
+  G_APPLICATION_CLASS(klass)->activate = fl_application_activate;
+}
 
-  return 0;
+static void fl_application_init(FlApplication* self) {}
+
+static FlApplication* fl_application_new() {
+  return FL_APPLICATION(g_object_new(fl_application_get_type(), nullptr));
+}
+
+int main(int argc, char** argv) {
+  g_autoptr(FlApplication) app = fl_application_new();
+  return g_application_run(G_APPLICATION(app), argc, argv);
 }
