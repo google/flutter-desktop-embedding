99b2f4c74727f7795a72b05cb6d5895fc4a5b0d3
Robert Ancell <robert.ancell@canonical.com>
2020-06-10 10:06:04 +1200

    Add GTK url_launcher plugin
    Fixes https://github.com/flutter/flutter/issues/41721

--- testbed/gtk/flutter/gtk_plugin_registrant.cc
+++ testbed/gtk/flutter/gtk_plugin_registrant.cc
@@ -6,6 +6,7 @@
 #include <color_panel/color_panel_plugin.h>
 #include <file_chooser/file_chooser_plugin.h>
 #include <menubar/menubar_plugin.h>
+#include <url_launcher/url_launcher_plugin.h>
 #include <window_size/window_size_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
@@ -18,6 +19,9 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) menubar_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "MenubarPlugin");
   menubar_plugin_register_with_registrar(menubar_registrar);
+  g_autoptr(FlPluginRegistrar) url_launcher_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
+  url_launcher_plugin_register_with_registrar(url_launcher_registrar);
   file_chooser_plugin_register_with_registrar(file_chooser_registrar);
   g_autoptr(FlPluginRegistrar) window_size_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "WindowSizePlugin");
