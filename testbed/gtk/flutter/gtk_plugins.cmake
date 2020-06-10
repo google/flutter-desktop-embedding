ba7691cc4e75062fce5b327a23076f2e2553a244
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2020-06-10 22:38:36 +0100

    Remove use of BundleUtilities in the GTK runner

--- testbed/gtk/flutter/gtk_plugins.cmake
+++ testbed/gtk/flutter/gtk_plugins.cmake
@@ -7,7 +7,11 @@ list(APPEND FLUTTER_PLUGIN_LIST
   window_size
 )
 
+set(PLUGIN_BUNDLED_LIBRARIES)
+
 foreach(plugin ${FLUTTER_PLUGIN_LIST})
   add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/gtk plugins/${plugin})
   target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
+  list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
+  list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
 endforeach(plugin)
