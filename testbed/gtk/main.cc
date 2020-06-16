736eff6af716aff87ab256bfb27a6958c1486cba
Robert Ancell <robert.ancell@canonical.com>
2020-06-17 07:05:18 +1200

    Remove headless from the template - only custom apps will make use of this

--- testbed/gtk/main.cc
+++ testbed/gtk/main.cc
@@ -1,13 +1,6 @@
 #include "fl_application.h"
-#include "fl_headless_application.h"
 
 int main(int argc, char** argv) {
-  g_autoptr(GApplication) app = nullptr;
-
-  if (gdk_init_check(&argc, &argv))
-    app = G_APPLICATION(fl_application_new());
-  else
-    app = G_APPLICATION(fl_headless_application_new());
-
-  return g_application_run(app, argc, argv);
+  g_autoptr(FlApplication) app = fl_application_new();
+  return g_application_run(G_APPLICATION(app), argc, argv);
 }
