0767d4ebc948949566bb0884b8c1deed6f653853
Stuart Morgan <stuartmorgan@google.com>
2020-06-16 12:28:42 -0700

    Update generated files from build run

--- testbed/linux/flutter/generated_plugins.cmake
+++ testbed/linux/flutter/generated_plugins.cmake
@@ -1,17 +1,19 @@
-# Once the Flutter tooling switches to use GTK, this will be generated automatically.
-# For now, plugins must be added here manually.
+#
+# Generated file, do not edit.
+#
+
 list(APPEND FLUTTER_PLUGIN_LIST
   color_panel
   file_chooser
-  menubar
   url_launcher_fde
+  menubar
   window_size
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
 
 foreach(plugin ${FLUTTER_PLUGIN_LIST})
-  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/gtk plugins/${plugin})
+  add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/linux plugins/${plugin})
   target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
