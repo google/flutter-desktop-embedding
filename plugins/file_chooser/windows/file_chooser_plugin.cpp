7b819074cb9aeebbc1c61d265533574512f8078d
Stuart Morgan <stuartmorgan@google.com>
2020-06-25 21:25:58 -0700

    Get everything building

--- plugins/file_chooser/windows/file_chooser_plugin.cpp
+++ plugins/file_chooser/windows/file_chooser_plugin.cpp
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "file_chooser_plugin.h"
+#include "include/file_chooser/file_chooser_plugin.h"
 
 #include <ShObjIdl_core.h>
 #include <flutter/flutter_view.h>
