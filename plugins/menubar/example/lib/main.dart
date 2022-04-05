0724a21519dd931cfedc0bfa8b7f972ecb0c0adc
Greg Spencer <gspencer@google.com>
2022-04-05 10:49:38 -0700

    Add copyrights

--- plugins/menubar/example/lib/main.dart
+++ plugins/menubar/example/lib/main.dart
@@ -1,62 +1,138 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Flutter code sample for PlatformMenuBar
+
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:flutter/services.dart';
-import 'package:menubar/menubar.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const SampleApp());
+
+enum MenuSelection {
+  about,
+  showMessage,
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SampleApp extends StatelessWidget {
+  const SampleApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _menubarPlugin = Menubar();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: MyMenuBarApp()),
+    );
   }
+}
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _menubarPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+class MyMenuBarApp extends StatefulWidget {
+  const MyMenuBarApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyMenuBarApp> createState() => _MyMenuBarAppState();
+}
+
+class _MyMenuBarAppState extends State<MyMenuBarApp> {
+  String _message = 'Hello';
+  bool _showMessage = false;
+
+  void _handleMenuSelection(MenuSelection value) {
+    switch (value) {
+      case MenuSelection.about:
+        showAboutDialog(
+          context: context,
+          applicationName: 'MenuBar Sample',
+          applicationVersion: '1.0.0',
+        );
+        break;
+      case MenuSelection.showMessage:
+        setState(() {
+          _showMessage = !_showMessage;
+        });
+        break;
     }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
+    // This builds a menu hierarchy that looks like this:
+    // Flutter API Sample
+    //  ├ About
+    //  ├ ────────  (group divider)
+    //  ├ Hide/Show Message
+    //  ├ Messages
+    //  │  ├ I am not throwing away my shot.
+    //  │  └ There's a million things I haven't done, but just you wait.
+    //  └ Quit
+    return PlatformMenuBar(
+      menus: <MenuItem>[
+        PlatformMenu(
+          label: 'Flutter API Sample',
+          menus: <MenuItem>[
+            PlatformMenuItemGroup(
+              members: <MenuItem>[
+                PlatformMenuItem(
+                  label: 'About',
+                  onSelected: () {
+                    _handleMenuSelection(MenuSelection.about);
+                  },
+                )
+              ],
+            ),
+            PlatformMenuItemGroup(
+              members: <MenuItem>[
+                PlatformMenuItem(
+                  onSelected: () {
+                    _handleMenuSelection(MenuSelection.showMessage);
+                  },
+                  shortcut: const CharacterActivator('m'),
+                  label: _showMessage ? 'Hide Message' : 'Show Message',
+                ),
+                PlatformMenu(
+                  label: 'Messages',
+                  menus: <MenuItem>[
+                    PlatformMenuItem(
+                      label: 'I am not throwing away my shot.',
+                      shortcut: const SingleActivator(LogicalKeyboardKey.digit1, meta: true),
+                      onSelected: () {
+                        setState(() {
+                          _message = 'I am not throwing away my shot.';
+                        });
+                      },
+                    ),
+                    PlatformMenuItem(
+                      label: "There's a million things I haven't done, but just you wait.",
+                      shortcut: const SingleActivator(LogicalKeyboardKey.digit2, meta: true),
+                      onSelected: () {
+                        setState(() {
+                          _message = "There's a million things I haven't done, but just you wait.";
+                        });
+                      },
+                    ),
+                  ],
+                )
+              ],
+            ),
+            if (PlatformProvidedMenuItem.hasMenu(PlatformProvidedMenuItemType.quit))
+              const PlatformProvidedMenuItem(type: PlatformProvidedMenuItemType.quit),
+          ],
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+      ],
+      body: Center(
+        child: Text(_showMessage
+            ? _message
+            : 'This space intentionally left blank.\n'
+            'Show a message here using the menu.'),
       ),
     );
   }
--- plugins/menubar/test/menubar_test.dart
+++ plugins/menubar/test/menubar_test.dart
@@ -1,3 +1,17 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:menubar/menubar.dart';
 import 'package:menubar/menubar_platform_interface.dart';
@@ -21,9 +35,11 @@ void main() {
 
   test('getPlatformVersion', () async {
     Menubar menubarPlugin = Menubar();
+    MenubarPlatform old = MenubarPlatform.instance;
     MockMenubarPlatform fakePlatform = MockMenubarPlatform();
     MenubarPlatform.instance = fakePlatform;
   
     expect(await menubarPlugin.getPlatformVersion(), '42');
+    MenubarPlatform.instance = old;
   });
 }
