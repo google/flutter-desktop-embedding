4bc2c310e8acf3740ea0c2b326b3ef71280391f8
stuartmorgan <stuartmorgan@google.com>
2022-08-16 11:19:16 -0400

    [file_selector] Add Linux unit tests (#904)
    Part one of preparation to move this implementation to flutter/plugins. Unlike the previous moves, I'm adding tests before moving, as it makes reviewing the deltas easier; the final move can make only the minor changes needed for the move itself instead of having functional diffs as well.
    
    Since the tests run in the context of the example app, and since there are no longer multiple implementations in this repo to share an example between, this moves the example app into the package.
    
    The testing here isn't as complete as I would like; it's testing internals that cover a significant portion but definitely not all of the native logic. This is a balance between getting some meaningful testing in place (and thus allowing moving and endorsing it) against the amount of time it would take me to fully test (ramping up on doing mocking, callback wrapper patterns, etc. in GObject).
    
    (Part two will add a Dart implementation to move the method channel in-package.)

--- plugins/file_selector/file_selector_linux/linux/file_selector_plugin.cc
+++ plugins/file_selector/file_selector_linux/linux/file_selector_plugin.cc
@@ -7,6 +7,8 @@
 #include <flutter_linux/flutter_linux.h>
 #include <gtk/gtk.h>
 
+#include "file_selector_plugin_private.h"
+
 // From method_channel_file_selector.dart
 const char kChannelName[] = "plugins.flutter.io/file_selector";
 
@@ -86,29 +88,15 @@ static GtkFileFilter* type_group_to_filter(FlValue* value) {
   return GTK_FILE_FILTER(g_object_ref(filter));
 }
 
-// Shows the requested dialog type.
-static FlMethodResponse* show_dialog(FlFileSelectorPlugin* self,
-                                     GtkFileChooserAction action,
-                                     bool choose_directory, const gchar* title,
-                                     const gchar* default_confirm_button_text,
-                                     FlValue* properties) {
-  if (fl_value_get_type(properties) != FL_VALUE_TYPE_MAP) {
-    return FL_METHOD_RESPONSE(fl_method_error_response_new(
-        kBadArgumentsError, "Argument map missing or malformed", nullptr));
-  }
-
+// Creates a GtkFileChooserNative for the given method call details.
+static GtkFileChooserNative* create_dialog(
+    GtkWindow* window, GtkFileChooserAction action, const gchar* title,
+    const gchar* default_confirm_button_text, FlValue* properties) {
   const gchar* confirm_button_text = default_confirm_button_text;
   FlValue* value = fl_value_lookup_string(properties, kConfirmButtonTextKey);
   if (value != nullptr && fl_value_get_type(value) == FL_VALUE_TYPE_STRING)
     confirm_button_text = fl_value_get_string(value);
 
-  FlView* view = fl_plugin_registrar_get_view(self->registrar);
-  if (view == nullptr) {
-    return FL_METHOD_RESPONSE(
-        fl_method_error_response_new(kNoScreenError, nullptr, nullptr));
-  }
-
-  GtkWindow* window = GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view)));
   g_autoptr(GtkFileChooserNative) dialog =
       GTK_FILE_CHOOSER_NATIVE(gtk_file_chooser_native_new(
           title, window, action, confirm_button_text, "_Cancel"));
@@ -117,11 +105,6 @@ static FlMethodResponse* show_dialog(FlFileSelectorPlugin* self,
   if (value != nullptr && fl_value_get_type(value) == FL_VALUE_TYPE_BOOL) {
     gtk_file_chooser_set_select_multiple(GTK_FILE_CHOOSER(dialog),
                                          fl_value_get_bool(value));
-  }
-
-  if (choose_directory) {
-    gtk_file_chooser_set_action(GTK_FILE_CHOOSER(dialog),
-                                GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER);
   }
 
   value = fl_value_lookup_string(properties, kInitialDirectoryKey);
@@ -142,17 +125,63 @@ static FlMethodResponse* show_dialog(FlFileSelectorPlugin* self,
       FlValue* type_group = fl_value_get_list_value(value, i);
       GtkFileFilter* filter = type_group_to_filter(type_group);
       if (filter == nullptr) {
-        return FL_METHOD_RESPONSE(fl_method_error_response_new(
-            kBadArgumentsError, "Allowed file types malformed", nullptr));
+        return nullptr;
       }
       gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
     }
   }
 
+  return GTK_FILE_CHOOSER_NATIVE(g_object_ref(dialog));
+}
+
+// TODO(stuartmorgan): Move this logic back into method_call_cb once
+// https://github.com/flutter/flutter/issues/88724 is fixed, and test
+// through the public API instead. This only exists to move as much
+// logic as possible behind the private entry point used by unit tests.
+GtkFileChooserNative* create_dialog_for_method(GtkWindow* window,
+                                               const gchar* method,
+                                               FlValue* properties) {
+  if (strcmp(method, kOpenFileMethod) == 0) {
+    return create_dialog(window, GTK_FILE_CHOOSER_ACTION_OPEN, "Open File",
+                         "_Open", properties);
+  } else if (strcmp(method, kGetDirectoryPathMethod) == 0) {
+    return create_dialog(window, GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
+                         "Choose Directory", "_Open", properties);
+  } else if (strcmp(method, kGetSavePathMethod) == 0) {
+    return create_dialog(window, GTK_FILE_CHOOSER_ACTION_SAVE, "Save File",
+                         "_Save", properties);
+  }
+  return nullptr;
+}
+
+// Shows the requested dialog type.
+static FlMethodResponse* show_dialog(FlFileSelectorPlugin* self,
+                                     const gchar* method, FlValue* properties,
+                                     bool return_list) {
+  if (fl_value_get_type(properties) != FL_VALUE_TYPE_MAP) {
+    return FL_METHOD_RESPONSE(fl_method_error_response_new(
+        kBadArgumentsError, "Argument map missing or malformed", nullptr));
+  }
+
+  FlView* view = fl_plugin_registrar_get_view(self->registrar);
+  if (view == nullptr) {
+    return FL_METHOD_RESPONSE(
+        fl_method_error_response_new(kNoScreenError, nullptr, nullptr));
+  }
+  GtkWindow* window = GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(view)));
+
+  g_autoptr(GtkFileChooserNative) dialog =
+      create_dialog_for_method(window, method, properties);
+
+  if (dialog == nullptr) {
+    return FL_METHOD_RESPONSE(fl_method_error_response_new(
+        kBadArgumentsError, "Unable to create dialog from arguments", nullptr));
+  }
+
   gint response = gtk_native_dialog_run(GTK_NATIVE_DIALOG(dialog));
   g_autoptr(FlValue) result = nullptr;
   if (response == GTK_RESPONSE_ACCEPT) {
-    if (action == GTK_FILE_CHOOSER_ACTION_OPEN && !choose_directory) {
+    if (return_list) {
       result = fl_value_new_list();
       g_autoptr(GSList) filenames =
           gtk_file_chooser_get_filenames(GTK_FILE_CHOOSER(dialog));
@@ -180,14 +209,10 @@ static void method_call_cb(FlMethodChannel* channel, FlMethodCall* method_call,
 
   g_autoptr(FlMethodResponse) response = nullptr;
   if (strcmp(method, kOpenFileMethod) == 0) {
-    response = show_dialog(self, GTK_FILE_CHOOSER_ACTION_OPEN, false,
-                           "Open File", "_Open", args);
-  } else if (strcmp(method, kGetDirectoryPathMethod) == 0) {
-    response = show_dialog(self, GTK_FILE_CHOOSER_ACTION_OPEN, true,
-                           "Choose Directory", "_Open", args);
-  } else if (strcmp(method, kGetSavePathMethod) == 0) {
-    response = show_dialog(self, GTK_FILE_CHOOSER_ACTION_SAVE, false,
-                           "Save File", "_Save", args);
+    response = show_dialog(self, method, args, true);
+  } else if (strcmp(method, kGetDirectoryPathMethod) == 0 ||
+             strcmp(method, kGetSavePathMethod) == 0) {
+    response = show_dialog(self, method, args, false);
   } else {
     response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
   }
