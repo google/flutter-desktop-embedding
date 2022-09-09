340b50291c915fcb989a2ac2d82a3d0bbebc4a97
Greg Spencer <gspencer@google.com>
2022-09-08 17:12:42 -0700

    checkpoint

--- testbed/macos/Flutter/GeneratedPluginRegistrant.swift
+++ testbed/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import menubar
 import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  MenubarPlugin.register(with: registry.registrar(forPlugin: "MenubarPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
