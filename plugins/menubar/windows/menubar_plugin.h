f8fe688fbee46882904e623725e40441668e3517
Greg Spencer <gspencer@google.com>
2022-10-17 11:59:13 -0700

    Works, no shortcuts

--- plugins/menubar/windows/menubar_plugin.h
+++ plugins/menubar/windows/menubar_plugin.h
@@ -3,8 +3,12 @@
 
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
+#include <flutter/basic_message_channel.h>
+#include <flutter/standard_method_codec.h>
+#include <flutter/encodable_value.h>
 
 #include <memory>
+#include <optional>
 
 namespace menubar {
 
@@ -22,15 +26,34 @@ class MenubarPlugin : public flutter::Plugin {
   MenubarPlugin(const MenubarPlugin&) = delete;
   MenubarPlugin& operator=(const MenubarPlugin&) = delete;
 
+  std::optional<flutter::EncodableValue> SetMenus(
+    const flutter::EncodableValue *arguments);
+
  private:
+  struct ShortcutEntry;
+
   // Called when a method is called on this plugin's channel from Dart.
   void HandleMethodCall(
       const flutter::MethodCall<flutter::EncodableValue> &method_call,
       std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 
-    flutter::PluginRegistrarWindows *registrar_;
-    std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel_;
-    uint32_t window_proc_id_;
+  std::optional<LRESULT> HandleWindowProc(HWND hwnd, UINT message,
+                                                    WPARAM wparam,
+                                                    LPARAM lparam);
+  std::optional<flutter::EncodableValue> PopulateMenu(
+    HMENU menu, const flutter::EncodableList &representation);
+
+  void CreateAccelerators();
+
+  std::optional<flutter::EncodableValue> AddMenuItem(
+    HMENU menu, const flutter::EncodableMap &representation);
+
+  flutter::PluginRegistrarWindows *registrar_;
+  std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel_;
+  uint32_t window_proc_id_;
+  std::map<int32_t, ShortcutEntry> shortcuts_;
+  std::map<int64_t, int64_t> shortcut_trigger_lookup_;
+  std::map<std::wstring, int64_t> shortcut_character_lookup_;
 };
 
 }  // namespace menubar
