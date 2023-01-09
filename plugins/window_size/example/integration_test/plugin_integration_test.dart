d74eba114147c13432aa523ee7ca427eb7a0e112
Stuart Morgan <stuartmorgan@google.com>
2023-01-09 14:06:38 -0500

    copyrights for integration tests

--- plugins/window_size/example/integration_test/plugin_integration_test.dart
+++ plugins/window_size/example/integration_test/plugin_integration_test.dart
@@ -1,10 +1,16 @@
-// This is a basic Flutter integration test.
+// Copyright 2018 Google LLC
 //
-// Since integration tests run in a full Flutter application, they can interact
-// with the host side of a plugin implementation, unlike Dart unit tests.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// For more information about Flutter integration tests, please see
-// https://docs.flutter.dev/cookbook/testing/integration/introduction
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
